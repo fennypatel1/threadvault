440a8f06ccbd139c7cd7d9f6df3bc3c4a5850a03
Fenny Patel <fapatel@uci.edu>
2026-04-14 11:40:06 -0700

    fix db connection

--- backend/db.py
+++ backend/db.py
@@ -13,4 +13,7 @@ import os
 import psycopg2
 
 def get_connection():
-    return psycopg2.connect(os.environ["DATABASE_URL"])
+    return psycopg2.connect(
+        os.environ["DATABASE_URL"],
+        sslmode="require"
+    )
